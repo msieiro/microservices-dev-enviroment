da591f26d1a115ec31cfbc87b66decf35fc37f00
Martin Sieiro <sieirogutierrezmartin@gmail.com>
2022-02-11 22:14:29 +0100

    style: renombrado recolocacion de archivos

--- backend/java-modules/accounts/src/main/java/com/msieiro/accounts/application/register/CreateAccountCommand.java
+++
@@ -1,6 +0,0 @@
-package com.msieiro.accounts.application.register;
-
-import com.msieiro.shared.domain.bus.command.Command;
-
-public record CreateAccountCommand(String id, String email, String password) implements Command {
-}
